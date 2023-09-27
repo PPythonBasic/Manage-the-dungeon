8c24b680fa28879656e4ba97b1d078e107189cf9
watchakorn-18k <74919942+watchakorn-18k@users.noreply.github.com>
2023-09-27 17:26:34 +0000

    Upadte

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 from manage_the_dungeon import *
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-  # run_dungeon()
-  add_items()
-  add_items()
-  add_items()
-  add_items()
-  view_item()
-=======
   run_dungeon()
   
->>>>>>> 569b3fc0fa371ec27f92aa6ab01b58569e7e3788

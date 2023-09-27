569b3fc0fa371ec27f92aa6ab01b58569e7e3788
KorVee12 <skybluekor@gmail.com>
2023-09-27 23:56:27 +0700

    All of function have done 100%.

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 from manage_the_dungeon import *
 
 if __name__ == "__main__":
-  # run_dungeon()
-  add_items()
-  add_items()
-  add_items()
-  add_items()
-  view_item()
-  enter_dungeon()
-  view_item()
+  run_dungeon()
+  
--- manage_the_dungeon.py
+++ manage_the_dungeon.py
@@ -1,41 +1,40 @@
 import random
+
 list_items = []
+
 
 def add_items():
     global list_items
     while True:
-        item = input('Enter Item name : ')
-        if item != '':
+        item = input("Enter Item name : ")
+        if item != "":
             if len(item) <= 2:
                 item = item.upper()
             else:
                 item = item.capitalize()
             break
     while True:
-        price = input('Enter Price : ')
-        if price != '':
+        price = input("Enter Price : ")
+        if price != "":
             price = int(price)
             break
     while True:
-        amount = input('Enter Amount : ')
-        if amount != '':
+        amount = input("Enter Amount : ")
+        if amount != "":
             amount = int(amount)
             break
 
-    dict_item = {
-        'Item_name': item, 
-        'Price': price, 
-        'Amount': amount
-    }
+    dict_item = {"Item_name": item, "Price": price, "Amount": amount}
     list_items.append(dict_item)
-    print('-------')
-    print(f'{item} {price}G {amount}x')
+    print("-------")
+    print(f"{item} {price}G {amount}x")
 
 
 def view_item():
-    print(' Show All Items in the Dungeon '.center(40,"-"))
+    print(" Show All Items in the Dungeon ".center(40, "-"))
     for i in list_items:
         print(f"- {i['Item_name']} {i['Price']}G {i['Amount']}x")
+
 
 def enter_dungeon():
     global list_items
@@ -45,36 +44,24 @@ def enter_dungeon():
     print(f"Hero receives {lost_item['Amount']}x {lost_item['Item_name']} item.")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def match_funct(choice:int):
+def match_funct(choice: int): 
     match choice:
         case 1:
-            pass
+            add_items()
+        case 2:
+            view_item()
+        case 3:
+            enter_dungeon()
+        case 0:
+            exit()
+
 
 def run_dungeon():
-
     while True:
-        print('<|Manage The Dungeon|>'.center(40," "))
-        print('[1] Add items to the dungeon')
-        print('[2] View items in the dungeon')
-        print('[3] Let the hero enter the dungeon.')
-        print('[0] Close the dungeon.')
-        choice = input('Choice : ')
+        print("<|Manage The Dungeon|>".center(40, " "))
+        print("[1] Add items to the dungeon")
+        print("[2] View items in the dungeon")
+        print("[3] Let the hero enter the dungeon.")
+        print("[0] Close the dungeon.")
+        choice = int(input("Choice : "))
         match_funct(choice)
